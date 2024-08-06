cf5bf7fb35df94b768b3e9904a5e6c414ed03b40
bigcat3997 <vqt.develop@gmail.com>
2024-08-06 13:36:12 +0700

    add new Platform model

---
+++ functions/models/platform_model.py
@@ -0,0 +1,20 @@
+import json
+from enum import Enum
+
+
+class Platform(Enum):
+    MAVEN = "MAVEN"
+    GRADLE = "GRADLE"
+    DOTNET = "DOTNET"
+    PYTHON = "PYTHON"
+    NPM = "NPM"
+    YARN = "YARN"
+
+    def __repr__(self):
+        return f"Platform(name={self.name})"
+
+    def to_dict(self):
+        return {"name": self.name}
+
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=4)
