3b94c476518815da03f565b15f9acb2412338e00
bigcat3997 <vqt.develop@gmail.com>
2024-12-14 08:10:53 +0700

    fixed create_jenkins_env_var func

--- app/services/jenkins_service.py
+++ app/services/jenkins_service.py
@@ -1,4 +1,5 @@
 def create_jenkins_env_var(env_vars, prefix_var: str = "FLOW_") -> None:
     with open("env_vars.sh", "w") as f:
-        for env_var, value in env_vars:
-            f.write(f'export {env_var}="{value}"\n')
+        for ev in env_vars:
+            for env_var, value in ev.items():
+                f.write(f'export {env_var}="{value}"\n')
