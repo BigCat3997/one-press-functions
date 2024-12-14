7e63c2a0e0a9a17a4c8af31791c212fdf1bd089e
bigcat3997 <vqt.develop@gmail.com>
2024-12-14 09:40:26 +0700

    upper name of env var

--- app/services/jenkins_service.py
+++ app/services/jenkins_service.py
@@ -2,4 +2,5 @@ def create_jenkins_env_var(env_vars, env_vars_path, prefix_var: str = "FLOW_") -
     with open(f"{env_vars_path}/env_vars.sh", "w") as f:
         for ev in env_vars:
             for env_var, value in ev.items():
-                f.write(f'export {env_var}="{value}"\n')
+                env_var = f"{prefix_var.upper()}{env_var.upper()}"
+                f.write(f'{env_var}="{value}"\n')
