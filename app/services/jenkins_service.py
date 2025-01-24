7e718a85a80fe33b85ad91c2ebe4ed0186a646d4
bigcat3997 <vqt.develop@gmail.com>
2025-01-24 12:03:14 +0700

    add export when create env var

--- app/services/jenkins_service.py
+++ app/services/jenkins_service.py
@@ -3,4 +3,5 @@ def create_jenkins_env_var(env_vars, env_vars_path, prefix_var: str = "FLOW_") -
         for ev in env_vars:
             for env_var, value in ev.items():
                 env_var = f"{prefix_var.upper()}{env_var.upper()}"
-                f.write(f'{env_var}="{value}"\n')
+                create_env_var_cmd = f'export {env_var}="{value}"\n'
+                f.write(create_env_var_cmd)
