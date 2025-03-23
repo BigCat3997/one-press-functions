c2594490c962c54cf02b752cd2b99a8ecf9dd142
bigcat3997 <vqt.develop@gmail.com>
2025-03-23 10:39:15 +0700

    refactor func

--- app/services/jenkins_service.py
+++ app/services/jenkins_service.py
@@ -1,6 +1,4 @@
-def create_jenkins_env_var(env_vars, env_vars_path, prefix_var: str = "FLOW_") -> None:
-    with open(f"{env_vars_path}/env_vars.sh", "w") as f:
-        for ev in env_vars:
-            for env_var, value in ev.items():
-                env_var = f"{prefix_var.upper()}{env_var.upper()}"
-                f.write(f'{env_var}="{value}"\n')
+def create_jenkins_env_var(env_vars, prefix_var: str = "FLOW_") -> None:
+    with open("env_vars.sh", "w") as f:
+        for env_var, value in env_vars.items():
+            f.write(f'export {env_var}="{value}"\n')
